c6a368fb491aa521244e192c6582e0fd9d62ff8e
Kellen Cheng <54748182+ktcheng@users.noreply.github.com>
2019-09-13 10:56:36 -0400

    Add files via upload

---
+++ Version 2/Complete/Models/Reminder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteList.Models
+{
+    public class Reminder
+    {
+        public string ReminderName { get; set; }
+        public string ReminderLocation { get; set; }
+        public string ReminderNotes { get; set; }
+        public string ReminderDate { get; set; }
+        public string ReminderTime { get; set; }
+        public DateTime ReminderNotification { get; set; }
+    }
+}
